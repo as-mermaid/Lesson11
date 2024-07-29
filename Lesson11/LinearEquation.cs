c981d0fd81704d758d64abaa6a211ff10560ef69
mermaid <mermaid@ANASTASIASMEA3D>
2024-07-29 16:59:18 +0200

    Добавьте файлы проекта.

---
+++ Lesson11/LinearEquation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    struct LinearEquation
+    {
+        double k;
+        double b;
+
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0 && b != 0)
+                return "Уравнение не имеет решения";
+            else if (k == 0 && b == 0)
+                return "Уравнение имеет бесконечное множество решений";
+            else
+                return $"Решение уравнение x={-b / k}";
+        }
+    }
+}
---
+++ Lesson11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для решение уравнения вида 0 = kx + b");
+            Console.Write("Введите значение коэффициента k=");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение коэффициента b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            LinearEquation equation = new LinearEquation(k,b);
+            Console.WriteLine(equation.Root());
+            Console.ReadKey();
+        }
+    }
+}
